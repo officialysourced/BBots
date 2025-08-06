0ce71750ae41aea3331bc6a7767234a5ee9c51dc
officialysourced1 <officialy.sourced@gmail.com>
2025-08-05 20:16:17 -0400

    Added some comments

--- src/autoncode.cpp
+++ src/autoncode.cpp
@@ -2,11 +2,12 @@
 #include "autoncode.hpp"
 
 void combinedAutonRoutine() {
+    // Do not delete these lines - they are needed for chassis control
     pros::Motor left_front_motor(1);
     pros::Motor left_back_motor(2);
     pros::Motor right_front_motor(3);
     pros::Motor right_back_motor(4);
-
+// Sample code: 
     left_front_motor.move(80);
     left_back_motor.move(80);
     right_front_motor.move(80);
